70fee95519e0c8ccff2ca5bc521c2c6df8990543
Hazem Kawas <hazem.k.4488@gmail.com>
2022-03-03 17:54:07 +0100

    fixing LS error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,44 +6,51 @@ import ChallengePage from "./Challenge/ChallengePage";
 import ProgressPage from "./Progress/ProgressPage";
 import StartPage from "./StartPage/StartPage";
 
+const loadFromLS = () => {
+  const localData = localStorage.getItem('challengeData');
+  if (localData) {
+    const user = JSON.parse(localData);
+    user.start = new Date(user.start)
+    return user;
+  }
+}
+
 function App() {
   const navigate = useNavigate()
-  const [challengeData, setChallengeData] = useState<User>();
+  const [challengeData, setChallengeData] = useState<User>(loadFromLS());
   const [_, setDate] = useState(new Date())
-  
+
   const newChallengeDataHandler = (enteredUserData: User) => {
     setChallengeData(enteredUserData);
     navigate("progressPage");
   };
 
   useEffect(() => {
+    console.log(challengeData);
+
     if (!challengeData) return;
     const { start } = challengeData;
     const timeoutDate = new Date(start)
     timeoutDate.setDate(start.getDate() + 1)
-    
+
     const diffTime = Math.abs(new Date().getTime() - timeoutDate.getTime());
     console.log(timeoutDate);
-    
+
     setTimeout(() => {
       setDate(new Date())
     }, diffTime);
   }, [])
-  
-  // useEffect(() => {
-  //   const localData = localStorage.getItem('challengeData');
-  //   if(localData) {
-  //     setChallengeData(JSON.parse(localData));
-  //   }
-  // }, [])
-  
+
+
+
+
+  useEffect(() => {
+    if (!challengeData) return;
+    localStorage.setItem('challengeData', JSON.stringify(challengeData))
+  });
+
   // useEffect(() => {
   //   if (!challengeData) return;
-  //   localStorage.setItem('challengeData', JSON.stringify(challengeData))
-  // });
-  
-  // useEffect(() => {
-  //   if(!challengeData) return;
   //   navigate("progressPage");
   // })
 
@@ -52,7 +59,7 @@ function App() {
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="challengePage" element={<ChallengePage saveNewChallengeData={newChallengeDataHandler} />} />
-        <Route path="progressPage" element={<ProgressPage userData={challengeData}/>} />
+        <Route path="progressPage" element={<ProgressPage userData={challengeData} />} />
         <Route path="*" element={<div style={{ position: "absolute", left: '50%', top: '50%', transform: 'translate(-50%)', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '4rem' }}>404</div>} />
       </Routes>
     </div>
